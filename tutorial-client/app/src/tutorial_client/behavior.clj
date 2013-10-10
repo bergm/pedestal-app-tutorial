27ce5486a5d14557259d50e52ec3f2ab04f68eed
Michael Berg <michael.berg@zalf.de>
2013-10-10 16:47:47 +0200

    initial commit

---
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -0,0 +1,45 @@
+(ns ^:shared tutorial-client.behavior
+    (:require [clojure.string :as string]
+              [io.pedestal.app.messages :as msg]
+              [io.pedestal.app :as app]))
+
+;; transform functions
+(defn inc-transform [old-value _]
+  ((fnil inc 0) old-value))
+
+(defn swap-transform [_ message]
+  (:value message))
+
+;; derive functions
+(defn total-count [_ nums] (apply + nums))
+
+(defn maximum [old-value nums]
+  (apply max (or old-value 0) nums))
+
+;; effect functions
+(defn publish-counter [count]
+  [{msg/type :swap msg/topic [:other-counters] :value count}])
+
+;; emit functions
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
+;; app dataflow
+(def example-app
+  {:version 2
+
+   :transform [[:inc [:my-counter] inc-transform]
+               [:swap [:**]        swap-transform]]
+
+   :emit [{:init init-main}
+          [#{[:my-counter]
+             [:other-counters :*]
+             [:total-count]
+             [:max-count]} (app/default-emitter [:main])]]
+
+   :effect #{[#{[:my-counter]} publish-counter :single-val]}
+
+   :derive #{[#{[:my-counter] [:other-counters :*]} [:total-count] total-count :vals]
+             [#{[:my-counter] [:other-counters :*]} [:max-count] maximum :vals]}})
+
+
---
+++ tutorial-client/test/tutorial_client/behavior_test.clj
@@ -0,0 +1,70 @@
+(ns tutorial-client.behavior-test
+  (:require [io.pedestal.app :as app]
+            [io.pedestal.app.protocols :as p]
+            [io.pedestal.app.tree :as tree]
+            [io.pedestal.app.messages :as msg]
+            [io.pedestal.app.render :as render]
+            [io.pedestal.app.util.test :as test])
+  (:use clojure.test
+        tutorial-client.behavior
+        [io.pedestal.app.query :only [q]]))
+
+;; Test a transform function
+
+#_(deftest test-set-value-transform
+  (is (= (set-value-transform {} {msg/type :set-value msg/topic [:greeting] :value "x"})
+         "x")))
+
+(deftest test-inc-transform
+  (is (= (inc-transform nil {msg/type :inc msg/topic [:my-counter]})
+         1))
+  (is (= (inc-transform 0 {msg/type :inc msg/topic [:my-counter]})
+         1))
+  (is (= (inc-transform 1 {msg/type :inc msg/topic [:my-counter]})
+         2))
+  (is (= (inc-transform 1 nil)
+         2)))
+
+
+;; Build an application, send a message to a transform and check the transform
+;; state
+
+(defn- data-model [app]
+  (-> app :state deref :data-model))
+
+(deftest test-app-state
+  (let [app (app/build example-app)]
+    (is (test/run-sync! app [{msg/type :inc msg/topic [:my-counter]}]
+                        :begin :default))
+    (is (= (data-model app)
+           {:my-counter 1})))
+  (let [app (app/build example-app)]
+    (is (test/run-sync! app [{msg/type :inc msg/topic [:my-counter]}
+                             {msg/type :inc msg/topic [:my-counter]}
+                             {msg/type :inc msg/topic [:my-counter]}]
+                        :begin :default))
+    (is (= (data-model app)
+           {:my-counter 3}))))
+
+#_(deftest test-app-state
+  (let [app (app/build example-app)]
+    (app/begin app)
+    (is (vector?
+         (test/run-sync! app [{msg/type :set-value msg/topic [:greeting] :value "x"}])))
+    (is (= (-> app :state deref :data-model :greeting) "x"))))
+
+;; Use io.pedestal.app.query to query the current application model
+
+#_(deftest test-query-ui
+  (let [app (app/build example-app)
+        app-model (render/consume-app-model app (constantly nil))]
+    (app/begin app)
+    (is (test/run-sync! app [{msg/topic [:greeting] msg/type :set-value :value "x"}]))
+    (is (= (q '[:find ?v
+                :where
+                [?n :t/path [:greeting]]
+                [?n :t/value ?v]]
+              @app-model)
+           [["x"]]))))
+
+
